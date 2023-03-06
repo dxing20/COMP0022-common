22b5970925c6f7a2c9853932cf36d8803a26f279
Danny Xing <danny.xing.20@ucl.ac.uk>
2023-03-06 11:15:48 +0000

    added request validation result checker

---
+++ src/errors/request-validation-error.ts
@@ -0,0 +1,17 @@
+import { ValidationError } from "express-validator";
+import { DefinedError } from "./defined-error";
+
+export class RequestValidationError extends DefinedError {
+  statusCode = 404;
+  validationErrors: { message: string; field: string }[];
+
+  constructor(validationErrors: { message: string; field: string }[]) {
+    super("The page you are looking for does not exist");
+    Object.setPrototypeOf(this, RequestValidationError.prototype);
+    this.validationErrors = validationErrors;
+  }
+
+  response() {
+    return this.validationErrors;
+  }
+}
